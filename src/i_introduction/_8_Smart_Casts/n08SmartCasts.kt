a475335c8469411dfb2aedb1ba219acb578da179
johanm <johanm@netcompany.com>
2018-02-04 12:46:37 +0100

    Reformat on task 8

--- src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
@@ -6,6 +6,7 @@ import util.doc8
 // 'sealed' modifier restricts the type hierarchy:
 // all the subclasses must be declared in the same file
 sealed class Expr
+
 class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 
@@ -16,11 +17,11 @@ fun eval(e: Expr): Int =
         }
 
 fun todoTask8(expr: Expr): Nothing = TODO(
-    """
+        """
         Task 8.
         Complete the implementation of the 'eval' function above using smart casts and 'when' expression.
         The 'JavaCode8.eval' method provides the similar functionality written in Java.
     """,
-    documentation = doc8(),
-    references = { JavaCode8().eval(expr) })
+        documentation = doc8(),
+        references = { JavaCode8().eval(expr) })
 
